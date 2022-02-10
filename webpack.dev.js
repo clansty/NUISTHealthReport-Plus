a7a1121c1d4b64b1e5ec4ff89a28ede6efa700b5
凌莞 <i@gao4.pw>
2022-02-10 16:14:20 +0800

    perf: import `version, author, source, license` from `package.json`

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const { TampermonkeyWebpackPlugin } = require('tampermonkey-webpack-plugin');
+const { version, author, source, license } = require('./package.json');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -11,9 +12,11 @@ module.exports = merge(common, {
     new TampermonkeyWebpackPlugin({
       minAlignSpace: 2,
       header: {
-        author: 'lixiang810 & Clansty',
+        author,
         name: ['NUISTHealthReport', ['zh', '南京信息工程大学每日健康申报']],
-        version: '0.114.514',
+        source,
+        version,
+        license,
         include: 'http://i.nuist.edu.cn/qljfwapp/sys/lwNuistHealthInfoDailyClock/*',
         icon: 'https://github.githubassets.com/images/icons/emoji/unicode/1f389.png',
         grant: ['unsafeWindow', 'GM_setValue', 'GM_setValue', 'GM_xmlhttpRequest'],
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const { TampermonkeyWebpackPlugin } = require('tampermonkey-webpack-plugin');
+const { version, author, source, license } = require('./package.json');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -8,9 +9,11 @@ module.exports = merge(common, {
     new TampermonkeyWebpackPlugin({
       minAlignSpace: 2,
       header: {
-        author: 'lixiang810 & Clansty',
+        author,
         name: ['NUISTHealthReport', ['zh', '南京信息工程大学每日健康申报']],
-        version: '0.114.514',
+        version,
+        source,
+        license,
         include: 'http://i.nuist.edu.cn/qljfwapp/sys/lwNuistHealthInfoDailyClock/*',
         icon: 'https://github.githubassets.com/images/icons/emoji/unicode/1f389.png',
         grant: ['unsafeWindow', 'GM_setValue', 'GM_setValue', 'GM_xmlhttpRequest'],
