a05887c4ba2fe9c599e7a0b2072e75bd554fc53e
凌莞 <i@gao4.pw>
2022-02-10 15:56:38 +0800

    feat: 一键自选日期签到

---
+++ src/doCheckin.ts
@@ -0,0 +1,33 @@
+import IRecord from '../types/IRecord';
+import request from './controller/request';
+import getTemp from './utils/getTemp';
+import getWID from './utils/getWID';
+import timeUtils from './utils/timeUtils';
+
+const doCheckin = async (now: Date) => {
+  const savedReports = await request.getMyDailyReport();
+  const lastReport: IRecord = savedReports.rows[0];
+  console.log(savedReports);
+  const WID = getWID(lastReport.USER_ID, now);
+  console.log(WID === lastReport.WID);
+  const time = timeUtils.getyyyyMMddHHmmss(now);
+  const date = timeUtils.getyyyyMMdd(now);
+  const reportToSend: IRecord = {
+    ...lastReport,
+    CREATED_AT: time,
+    CZRQ: time,
+    FILL_TIME: time,
+    NEED_CHECKIN_DATE: date,
+    WID,
+    TODAY_TEMPERATURE: getTemp().toString(),
+  };
+  console.log(reportToSend);
+  // 第一次（创建）时会强制设置日期，所以再来一次覆盖就好
+  await request.HealthDailyInfoSave(reportToSend);
+  const res = await request.HealthDailyInfoSave(reportToSend);
+  console.log(res);
+  console.log('签到完毕');
+  alert('签到完毕');
+};
+
+export default doCheckin;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 /* eslint-disable no-console */
 /* eslint-disable no-await-in-loop */
 import delay from 'delay';
-import IRecord from '../types/IRecord';
-import request from './controller/request';
-import getTemp from './utils/getTemp';
-import getWID from './utils/getWID';
+import doCheckin from './doCheckin';
 import timeUtils from './utils/timeUtils';
 
 (async ($) => {
@@ -12,35 +9,18 @@ import timeUtils from './utils/timeUtils';
     await delay(500);
   }
   delay(1000);
-  const btn = document.createElement('div');
-  btn.className = 'bh-btn bh-btn-primary';
-  btn.innerText = '一键签到（今日）';
-  $('div.bh-btn-default[data-action=showHistory]').after(btn);
-  btn.onclick = async () => {
-    const savedReports = await request.getMyDailyReport();
-    const lastReport: IRecord = savedReports.rows[0];
-    const now = new Date();
-    console.log(savedReports);
-    const WID = getWID(lastReport.USER_ID, now);
-    console.log(WID === lastReport.WID);
-    const time = timeUtils.getyyyyMMddHHmmss(now);
-    const date = timeUtils.getyyyyMMdd(now);
-    const reportToSend: IRecord = {
-      ...lastReport,
-      CREATED_AT: time,
-      CZRQ: time,
-      FILL_TIME: time,
-      NEED_CHECKIN_DATE: date,
-      WID,
-      TODAY_TEMPERATURE: getTemp().toString(),
-    };
-    console.log(reportToSend);
-    // 第一次（创建）时会强制设置日期，所以再来一次覆盖就好
-    await request.HealthDailyInfoSave(reportToSend);
-    const res = await request.HealthDailyInfoSave(reportToSend);
-    console.log(res);
-    console.log('签到完毕');
-    alert('签到完毕');
+  const datePicker = document.createElement('input');
+  datePicker.style.marginLeft = '4px';
+  datePicker.type = 'datetime-local';
+  datePicker.value = timeUtils.getyyyyMMddHHmmss(new Date()).replace(' ', 'T');
+  const btnCheckin = document.createElement('div');
+  btnCheckin.className = 'bh-btn bh-btn-primary';
+  btnCheckin.innerText = '一键签到';
+  btnCheckin.onclick = async () => {
+    const now = new Date(datePicker.value);
+    console.log(now);
+    await doCheckin(now);
   };
+  $('div.bh-btn-default[data-action=showHistory]').after(datePicker, btnCheckin);
   return 0;
 })(jQuery);
